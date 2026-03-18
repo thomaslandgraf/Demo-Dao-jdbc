b6df3df16eb0ca865e08d4d4d30633aac73762b8
Thomas Landgraf <thomas.ml@hotmail.com>
2026-03-18 00:12:09 -0700

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,13 @@
 import model.entities.Department;
+import model.entities.Seller;
 
 void main() {
 
     Department obj = new Department(1, "Books");
     IO.println(obj);
+
+    LocalDate date = LocalDate.now();
+    Seller seller = new Seller(1, "Bob", "bob@gmail.com", date, 2500.0, obj);
+
+    IO.println(seller);
 }
