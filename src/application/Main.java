4ce809f6430410f10899feb89dfa4a2957d748b8
Thomas Landgraf <thomas.ml@hotmail.com>
2026-03-18 21:59:35 -0700

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -5,6 +7,8 @@ void main() {
 
     Department obj = new Department(1, "Books");
     IO.println(obj);
+
+    SellerDao sellerDao = DaoFactory.createSellerDao();
 
     LocalDate date = LocalDate.now();
     Seller seller = new Seller(1, "Bob", "bob@gmail.com", date, 2500.0, obj);
