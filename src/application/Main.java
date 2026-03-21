32a7c18cfa24a5dc5d048c21b40bf3e3c8d0bbeb
Thomas Landgraf <thomas.ml@hotmail.com>
2026-03-20 23:47:32 -0700

    DepartmentDaoJDBC impl

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,4 +1,5 @@
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -8,6 +9,7 @@ void main() {
     Scanner sc = new Scanner(System.in);
 
     SellerDao sellerDao = DaoFactory.createSellerDao();
+    DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
     IO.println("===TEST 1: Seller findById===");
     Seller seller = sellerDao.findById(4);
@@ -47,6 +49,41 @@ void main() {
     int id = sc.nextInt();
     sc.nextLine();
     sellerDao.deleteById(id);
+    System.out.println("DELETE COMPLETE!");
+
+    IO.println();
+    IO.println("-------------------------DEPARTMENT-----------------");
+    IO.println("===TEST 1: Department findById===");
+    Department department = departmentDao.findById(4);
+    IO.println(department);
+
+    List<Department> departmentList = new ArrayList<>();
+    IO.println();
+    IO.println("===TEST 2: Find Department findAll===");
+    departmentList = departmentDao.findAll();
+    for(Department obj : departmentList) {
+        System.out.println(obj);
+    }
+
+    IO.println();
+    IO.println("===TEST 3: Department Insert===");
+    Department newDepartment = new Department(null, "Cashier");
+    departmentDao.insert(newDepartment);
+    IO.println("Inserted! New id = " + newDepartment.getId());
+
+    IO.println();
+    IO.println("===TEST 4: Department UPDATE===");
+    department = departmentDao.findById(1);
+    department.setName("Administration");
+    departmentDao.update(department);
+    IO.println("Update complete!");
+
+    IO.println();
+    IO.println("===TEST 5: Department DELETE===");
+    System.out.print("Enter id for delete test: ");
+    int dep_id = sc.nextInt();
+    sc.nextLine();
+    departmentDao.deleteById(dep_id);
     System.out.println("DELETE COMPLETE!");
 
     sc.close();
